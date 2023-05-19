9ff82234c240e4776beef0484a800d6323aba5c9
m4ym4y <m4ym4y@users.noreply.github.com>
2023-05-18 18:54:53 -0700

    fix piping

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ import Encodr from 'encodr'
 
 const CBOR = new Encodr('cbor')
 
+/* stolen code from https://github.com/nodejs/node/issues/947#issuecomment-119467568 */
+blackholeEPIPE(process.stderr)
+blackholeEPIPE(process.stdout)
+function blackholeEPIPE(stream) {
+  stream.on('error', onerror)
+  function onerror(err) {
+    if (err.code === 'EPIPE') {
+      stream._write = noopWrite
+      stream._writev = noopWritev
+      stream._read = noopRead
+      return stream.removeListener('error', onerror)
+    }
+    if (EE.listenerCount(stream, 'error') === 1) {
+      stream.removeListener('error', onerror)
+      stream.emit('error', err)
+    }
+  }
+}
+function noopWrite(chunk, enc, cb) {
+  cb()
+}
+function noopRead() {
+  this.push('')
+}
+function noopWritev(chunks, cb) {
+  cb()
+}
+/* end stolen code */
+
 async function example() {
   const inStream = fs.createReadStream(process.argv[2])
   const reader = await CarBlockIterator.fromIterable(inStream)
